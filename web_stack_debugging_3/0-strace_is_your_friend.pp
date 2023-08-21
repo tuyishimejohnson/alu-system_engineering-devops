b459abc888f1e04aa1c0db854b6586453c9e7f73
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-08-21 10:44:09 +0200

    Using strace

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,21 +1,11 @@
-# Using strace to find out why Apache is returning a 500 error
-file { '/path/to/apache/config/file':
-  ensure  => file,
-  content => template('my_module/apache_config.erb'),
-  notify  => Service['apache'],
-}
+# Using strace to debugg Apache returning 500 error
 
-service { 'apache':
-  ensure => running,
-  enable => true,
-}
-file { '/path/to/apache/config/file':
-  ensure  => file,
-  content => template('my_module/apache_config.erb'),
-  notify  => Service['apache'],
-}
+exec { 'new-state':
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path    => '/usr/local/bin/:/bin/'
+}# debug apache 500 server error
 
-service { 'apache':
-  ensure => running,
-  enable => true,
+exec { 'new-state':
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path    => '/usr/local/bin/:/bin/'
 }
