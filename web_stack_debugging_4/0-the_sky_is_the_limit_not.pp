22adabf0c25daf593f5cd2a8632e140bec52f35a
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-08-25 09:29:07 +0200

    Sky is the limit

---
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# Benchmarking localhost
+
+service {'nginx':
+    ensure => 'running'
+}
+
+exec { 'new-state':
+    command  => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/g" /etc/default/nginx',
+    provider => 'shell',
+    notify   => service['nginx']
+}
